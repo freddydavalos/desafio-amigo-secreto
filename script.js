83c4a11cff0f4e7a335df4a81c906206903d64ba
freddydavalos <freddydavalosmuriel@gmail.com>
2025-08-11 14:50:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("nombreAmigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    mostrarLista();
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let amigo of amigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML =
+        `🎉 El amigo secreto es: <strong>${amigoSecreto}</strong>`;
+}
